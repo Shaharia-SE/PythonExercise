ba751b30edbd436e7b9a4492e8c629715abb18fb
Shaharia-SE <shahariarony38@gmail.com>
2021-12-08 21:47:56 +0600

    Multi Line comment

---
+++ 05 Multi Line Comments.py
@@ -0,0 +1,12 @@
+# Multi line comment
+X = 5 #This is line comment
+Y = "Hi RONY"
+'''
+This is multiple line comment practice 
+Writen line 
+More then one line
+'''
+if 10==10 :
+    print("This is numeric number 10=10")
+    print(X)
+    print(Y)
