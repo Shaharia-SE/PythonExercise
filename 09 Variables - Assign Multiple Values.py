1d806e7099c3174fa53c3c73674092fe53700e91
Shaharia-SE <shahariarony38@gmail.com>
2021-12-09 10:41:41 +0600

    Python Variables - Assign Multiple Values

---
+++ 09 Variables - Assign Multiple Values.py
@@ -0,0 +1,17 @@
+# Many Values to Multiple Variables
+x, y, z = "Apple", "Banana", "Orange"
+print(x)
+print(y)
+print(z)
+
+# One Value to Multiple Variables
+A = B = C ="Mango"
+print(A)
+print(B)
+print(C)
+# Unpack a Collection
+fruits = ["Lemon", "Guava","jackfruit"]
+p,q,r=fruits
+print(p)
+print(q)
+print(r)
