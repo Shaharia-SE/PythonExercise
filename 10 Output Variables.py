c86adc6b533440a14293158889b993167ec8c783
Shaharia-SE <shahariarony38@gmail.com>
2021-12-09 10:59:32 +0600

    multi output variable

---
+++ 10 Output Variables.py
@@ -0,0 +1,17 @@
+# To combine both text and a variable, Python uses the + character
+X = "Awesome"
+print("Here is "+X)
+# use the + character to add a variable to another variable
+A = "Md.Shaharia Safat "
+B = "Rony"
+print(A+B)
+# the + character works as a mathematical operator
+P = 10
+Q = 15
+print(P+Q)
+'''
+If you try to combine a string and a number, Python will give you an error
+x = 5
+y = "John"
+print(x + y)
+'''
