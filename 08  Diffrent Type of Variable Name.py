9b5db52aae4b3057eb66072c62e06fd9d94278d3
Shaharia-SE <shahariarony38@gmail.com>
2021-12-09 09:25:36 +0600

    Variable Names

---
+++ 08  Diffrent Type of Variable Name.py
@@ -0,0 +1,28 @@
+''' This is diffrent type of variale  name '''
+
+myvar = "Rony"
+my_var = "Jony"
+_my_var = "Any"
+myVar = "Kony"
+MYVAR = "Sufka"
+myvar2 = "Tahazib"
+
+""" Illegal variable name 
+2myvar = "Rony"
+my-var = "Jony"
+my var = "any"
+"""
+# Bellow multi word variable name
+myVariableName = "Hi Shaharia Safat Rony"
+# Snake Case
+my_VariableName = "Hi Shaharia Safat Rony"
+print(myvar)
+print(my_var)
+print(_my_var)
+print(myVar)
+print(MYVAR)
+print(myvar2)
+print(myVariableName)
+print(my_VariableName)
+
+
