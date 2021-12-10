910501f067308461c97fdcb4d060feb98531c88e
Shaharia-SE <shahariarony38@gmail.com>
2021-12-10 19:11:57 +0600

    Assign String to a Variable Multiline Strings Strings are Arrays

---
+++ 17 Strings.py
@@ -0,0 +1,20 @@
+#Multiline Strings
+
+X= """Thank you for the opportunity to apply ,
+   for the Software Tester role at your company.
+   After reviewing your job description, 
+   it's clear that you're looking for a candidate
+   that is extremely familiar with the responsibilities
+   associated with the role and can perform them confidently. """
+print(X)
+Y = '''Thank you for the opportunity to apply ,
+   for the Software Tester role at your company.
+   After reviewing your job description, 
+   it's clear that you're looking for a candidate
+   that is extremely familiar with the responsibilities
+   associated with the role and can perform them confidently.'''
+print(Y)
+
+# Strings are Arrays
+a = "Md.Shaharia safat Rony!"
+print(a[4])
