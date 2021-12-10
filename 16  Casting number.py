a25268cb5c5d84fd383cc50925b236283c995925
Shaharia-SE <shahariarony38@gmail.com>
2021-12-10 18:53:16 +0600

    Specify a Variable Type

---
+++ 16  Casting number.py
@@ -0,0 +1,21 @@
+x = int(1)   # x will be 1
+y = int(2.8) # y will be 2
+z = int("3") # z will be 3
+print(x)
+print(y)
+print(z)
+A = float(1)     # x will be 1.0
+B = float(2.8)   # y will be 2.8
+C = float("3")   # z will be 3.0
+D = float("4.2") # w will be 4.2
+print(A)
+print(B)
+print(C)
+print(D)
+P = str("s1") # x will be 's1'
+Q = str(2)    # y will be '2'
+R = str(3.0)  # z will be '3.0'
+print(P)
+print(Q)
+print(R)
+
