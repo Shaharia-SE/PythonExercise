37e5fe6a09dda95d58fd6905af465fbd7f0afd78
Shaharia-SE <shahariarony38@gmail.com>
2021-12-10 19:48:47 +0600

    Slice From the Start Slice To the End And Negative

---
+++ 19 Slicing Strings.py
@@ -0,0 +1,12 @@
+# Get the characters from position 3 to position 6
+A = "Md.Shaharia!"  #The first character has index zero(0).
+print(A[3:6])
+
+B = "Shaharia Safat!"
+print(B[:8]) #Get the characters from the start to position 8 (not included):
+
+C = "Hello,Shaharia Rony"  #Get the characters from position 6, and all the way to the end:
+print(C[6:])
+
+b = "Hello, World!"
+print(b[-5:-2])
