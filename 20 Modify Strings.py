315c12bd99bcf7243f2a737d7d97ce86f1c12b7d
Shaharia-SE <shahariarony38@gmail.com>
2021-12-10 20:28:36 +0600

    Python - Modify Strings(upper lower,whitespace and others)

---
+++ 20 Modify Strings.py
@@ -0,0 +1,20 @@
+#Upper Case
+X = "shaharia safat rony"
+print(X.upper())
+Y = "SHAHARIA SAFAT RONY"
+print(Y.lower())
+
+#The strip() method removes any whitespace from the beginning or the end
+Z = "    Hi Shaharia Safat Rony! "
+print(Z.strip()) # returns "Hello, World!"
+
+#Replace String
+#The replace() method replaces a string with another string
+a = "Jhaharia, Rony"
+print(a.replace("J", "S"))
+
+
+#The split() method splits the string into substrings if it finds instances of the separator:
+a = "Shaharia, Rony"
+b = a.split(",")
+print(b)
