5db7607ac3e4641dc8320413256a6dd0a5ececc1
Shaharia-SE <shahariarony38@gmail.com>
2021-12-10 10:49:13 +0600

    Number and types

---
+++ 14 Diffrent type of Numbers.py
@@ -0,0 +1,55 @@
+#There are three numeric types in Python:
+x = 1
+y = 2.8
+z = 1j
+
+print(type(x))
+print(type(y))
+print(type(z))
+# Diffrent type of Integers
+A = 1
+B = 35656222554887711
+C = -3255522
+print(A)
+print(type(A))
+print(B)
+print(type(B))
+print(C)
+print(type(C))
+
+#diffrent type of float
+D = 1.10
+E = 1.0
+F = -35.59
+print(D)
+print(type(x))
+print(E)
+print(type(y))
+print(F)
+print(type(z))
+# Float can also be scientific numbers with an "e" to indicate the power of 10.
+G = 35e3
+H = 12E4
+I = -87.7e100
+print(G)
+print(type(G))
+print(H)
+print(type(H))
+print(I)
+print(type(I))
+
+#ValueErrorComplex numbers are written with a "j"
+K = 3+5j
+L = 5j
+M = -5j
+print(K)
+print(type(x))
+print(L)
+print(type(y))
+print(M)
+print(type(z))
+#Import the random module, and display a random number between 1 and 9:
+
+print("Random range : ")
+import random
+print(random.randrange(1,10))
