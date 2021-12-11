f7689825e96220363cd2907e832b60eb47b263ab
Shaharia-SE <shahariarony38@gmail.com>
2021-12-11 08:55:55 +0600

    To insert characters that are illegal in a string, use an escape character.

---
+++ 23 Escape Characters.py
@@ -0,0 +1,45 @@
+# to use double quotes when you normally would not be allowed
+txt = "We are food lover \"pizza\" from the pizza Hut."
+print(txt)
+
+#Single Quote
+txt = 'It\'s alright.'
+print(txt)
+
+#Backslash
+txt = "This will insert one \\ (backslash)."
+print(txt)
+
+#New Line
+txt = "Hello\nWorld!"
+print(txt)
+
+
+#Carriage Return
+txt = "Hello\rWorld!"
+print(txt)
+
+
+#Tab
+txt = "Hello\tWorld!"
+print(txt)
+
+
+#This example erases one character (backspace):
+txt = "Hello \bWorld!"
+print(txt)
+
+
+#A backslash followed by three integers will result in a octal value:
+txt = "\110\145\154\154\157"
+print(txt)
+
+
+#A backslash followed by three integers will result in a octal value:
+txt = "\110\145\154\154\157"
+print(txt)
+
+
+#A backslash followed by an 'x' and a hex number represents a hex value:
+txt = "\x48\x65\x6c\x6c\x6f"
+print(txt)
