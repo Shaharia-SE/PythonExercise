0ad0e6fbcab9fbf8bff87da08ae26e25b98dfff7
Shaharia-SE <shahariarony38@gmail.com>
2021-12-11 09:24:55 +0600

    You can create functions that returns a Boolean Value ,integer

---
+++ 25 Functions can Return a Boolean.py
@@ -0,0 +1,21 @@
+
+#Print the answer of a function
+def myFunction() :
+  return True
+
+print(myFunction())
+
+#Print "YES!" if the function returns True, otherwise print "NO!
+
+def myFunction1() :
+  return True
+
+if myFunction1():
+  print("YES!")
+else:
+  print("NO!")
+
+
+#Check if an object is an integer or not
+x = 200
+print(isinstance(x, int))
