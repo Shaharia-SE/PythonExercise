e716b57b5ddc991085bb045ba4f8b21d754f7465
Shaharia-SE <shahariarony38@gmail.com>
2021-12-13 10:19:10 +0600

    list item,length, duplicate,len etc

---
+++ 28 Python Lists.py
@@ -0,0 +1,43 @@
+#Create a List:
+Thislist = ["apple", "banana", "cherry"]
+print(Thislist)
+
+
+# list allow duplicate value
+thislist = ["apple", "banana", "cherry", "apple", "cherry"]
+print(thislist)
+
+#To determine how many items a list has, use the len() function
+
+thislist = ["apple", "banana", "cherry", "apple", "cherry"]
+print(len(thislist))
+
+'''
+List Items - Data Types
+List items can be of any data type:
+Example
+String, int and boolean data types:
+'''
+
+
+list1 = ["apple", "banana", "cherry"]
+list2 = [1, 5, 7, 9, 3]
+list3 = [True, False, False]
+print(list1)
+print(list2)
+print(list3)
+
+#A list with strings, integers and boolean values
+
+list1 = ["abc", 34, True, 40, "male"]
+print(list1)
+
+
+# is the data type of a list
+mylist = ["apple", "banana", "cherry"]
+print(type(mylist))
+
+# Using the list() constructor to make a List
+thislist = list(("apple", "banana", "cherry"))
+print(thislist)
+
