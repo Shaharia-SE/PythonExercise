8477c69fb6438c3fbb561c27e2c8d60ddd668f49
Shaharia-SE <shahariarony38@gmail.com>
2021-12-15 10:17:21 +0600

    Copy, list

---
+++ 36 Copy Lists.py
@@ -0,0 +1,9 @@
+#Make a copy of a list with the copy() method:
+
+thislist = ["apple","banana","cherry","orange","kiwi"]
+mylist = thislist.copy()
+print(mylist)
+
+#Make a copy of a list with the list() method
+mylist =list(thislist)
+print(mylist)
