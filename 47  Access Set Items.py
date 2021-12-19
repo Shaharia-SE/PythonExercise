fe003e5fde551f5f33d4284c76b4c89eeafdac00
Shaharia-SE <shahariarony38@gmail.com>
2021-12-19 19:23:50 +0600

    access item and check item

---
+++ 47  Access Set Items.py
@@ -0,0 +1,10 @@
+# Access Items
+# Loop through the set, and print the values
+thisset = {"apple","banana","cherry","orange","kiwi"}
+for x in thisset:
+    print(x)
+
+
+# Check if "banana" is present in the set
+
+print("banana" in thisset)
