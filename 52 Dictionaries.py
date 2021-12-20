f5952b600b8bcf199ff29326ea12d7a601258f32
Shaharia-SE <shahariarony38@gmail.com>
2021-12-20 20:09:27 +0600

    Dictionaries, length,

---
+++ 52 Dictionaries.py
@@ -0,0 +1,47 @@
+#Create and print a dictionary:
+
+thisdict = {
+    "brand" : "ford",
+    "model" : "mustag",
+    "year"  : 1964
+}
+print(thisdict)
+
+#Dictionary Items
+print("Print the 'brand' value of the dictionary:")
+
+print(thisdict["brand"])
+
+
+
+#Ordered or Unordered?
+
+thisdict = {
+    "brand" : "ford",
+    "model" : "Toyota",
+    "year" : 1964,
+    "year" : 2020
+}
+print(thisdict)
+
+#Dictionary Length
+#To determine how many items a dictionary has, use the len() function:
+
+print(len(thisdict))
+
+
+#Dictionary Items - Data Types
+print("String, int, boolean, and list data types:")
+thisdict ={
+    "brand" : "Bata",
+    "electricity" : False,
+    "year" : 2020,
+    "color" : ["red","yellow","green"]
+}
+print(thisdict)
+
+#Print the data type of a dictionary:
+
+print(type(thisdict))
+
+
