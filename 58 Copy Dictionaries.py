ceeffb9f90ec79b038753f72841e7d3785d4d1e5
Shaharia-SE <shahariarony38@gmail.com>
2021-12-20 22:28:00 +0600

    copy, dict

---
+++ 58 Copy Dictionaries.py
@@ -0,0 +1,18 @@
+# Make a copy of a dictionary with the copy() method
+thisdict = {
+    "brand": "Toyota",
+    "model": "szwl",
+    "year": 1964
+}
+mydct = thisdict.copy()
+print(mydct)
+
+# Make a copy of a dictionary with the dict() function
+
+thisdict = {
+    "brand": "Toyota",
+    "model": "szwl",
+    "year": 1964
+}
+mydict = dict(thisdict)
+print(mydct)
