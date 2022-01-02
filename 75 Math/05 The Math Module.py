b1f7b458553ea1ab67d2a9c1012b6fab16b75f5d
Shaharia-SE <shahariarony38@gmail.com>
2022-01-02 11:04:40 +0600

    upper, down number

---
+++ 75 Math/05 The Math Module.py
@@ -0,0 +1,8 @@
+import math
+
+# round a number upper to this nearest integer
+x = math.ceil(8.1)
+# round a number down to this nearest integer
+y = math.floor(5.9)
+print(x) #return 9
+print(y) # return 5
